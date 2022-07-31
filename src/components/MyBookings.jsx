33fed22cf05d561f5729edac4a019f817aa2ffa0
Donald Heal <donnie.heal@gmail.com>
2022-07-31 18:08:56 +1000

    bookings counter

--- src/components/MyBookings.jsx
+++ src/components/MyBookings.jsx
@@ -56,144 +56,139 @@ const AsyncAwait = () => {
     });
     window.location.reload();
   }
-  if (mybooking.length > 0) {
-    return (
-      <>
-        <div>
-          <div>
-            <h1 class="title">Your Bookings</h1>
-          </div>
-          {mybookings.length > 0 && (
-            <table border="1" style={{ float: "left" }}>
-              <tr>
-                <th>Student</th>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Location</th>
-                <th>Instrument</th>
-                <th>Operations</th>
-              </tr>
-              <tbody>
-                {mybookings.map((booking) => (
-                  <tr>
-                    <td>{booking.username}</td>
-                    <td>{booking.date}</td>
-                    <td>{booking.time}</td>
-                    <td>{booking.location}</td>
-                    <td>{booking.instrument}</td>
-                    <td>
-                      <button onClick={() => selectBooking(booking.id)}>
-                        Edit
-                      </button>
-                      <button onClick={() => deleteBooking(booking.id)}>
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-          <input type="hidden" value={user_id} />
-          <input type="hidden" value={username} />
-          <input type="hidden" value={id} />
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => {
-              setDate(e.target.value);
-            }}
-          />
-          <br></br>
-          <input
-            type="time"
-            value={time}
-            onChange={(e) => {
-              setTime(e.target.value);
-            }}
-          />
-          <br></br>
-          <label>Location:</label>
-          <label htmlFor="location">
-            <input
-              type="radio"
-              name="location"
-              id="location"
-              checked={location === "Studio"}
-              value="Studio"
-              onChange={(e) => {
-                setLocation(e.target.value);
-              }}
-            />
-            Studio
-          </label>
-          <label htmlFor="location">
-            <input
-              type="radio"
-              name="location"
-              id="location"
-              checked={location === "Online"}
-              value="Online"
-              onChange={(e) => {
-                setLocation(e.target.value);
-              }}
-            />
-            Online
-          </label>
-          <br></br>
-          <label>Instrument:</label>
-          <label htmlFor="instrument">
-            <input
-              type="radio"
-              name="instrument"
-              id="instrument"
-              value="Guitar"
-              checked={instrument === "Guitar"}
-              onChange={(e) => {
-                setInstrument(e.target.value);
-              }}
-            />
-            Guitar
-          </label>
-          <label htmlFor="instrument">
-            <input
-              type="radio"
-              name="instrument"
-              id="instrument"
-              value="Piano"
-              checked={instrument === "Piano"}
-              onChange={(e) => {
-                setInstrument(e.target.value);
-              }}
-            />
-            Piano
-          </label>
-          <label htmlFor="instrument">
-            <input
-              type="radio"
-              name="instrument"
-              id="instrument"
-              value="Voice"
-              checked={instrument === "Voice"}
-              onChange={(e) => {
-                setInstrument(e.target.value);
-              }}
-            />
-            Voice
-          </label>
-          <br></br>
-          <button onClick={updateBooking}> Edit Booking </button>
-        </div>
-      </>
-    );
-  } else {
-    return (
+  return (
+    <>
       <div>
-        <h1>You dont have any bookings to view</h1>
+        <div>
+          <h1 class="title">Your Bookings</h1>
+        </div>
+        {mybookings.length > 0 && (
+          <table border="1" style={{ float: "left" }}>
+            <tr>
+              <th>Student</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Location</th>
+              <th>Instrument</th>
+              <th>Operations</th>
+            </tr>
+            <tbody>
+              {mybookings.map((booking) => (
+                <tr>
+                  <td>{booking.username}</td>
+                  <td>{booking.date}</td>
+                  <td>{booking.time}</td>
+                  <td>{booking.location}</td>
+                  <td>{booking.instrument}</td>
+                  <td>
+                    <button onClick={() => selectBooking(booking.id)}>
+                      Edit
+                    </button>
+                    <button onClick={() => deleteBooking(booking.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        <input type="hidden" value={user_id} />
+        <input type="hidden" value={username} />
+        <input type="hidden" value={id} />
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+        />
+        <br></br>
+        <input
+          type="time"
+          value={time}
+          onChange={(e) => {
+            setTime(e.target.value);
+          }}
+        />
+        <br></br>
+        <label>Location:</label>
+        <label htmlFor="location">
+          <input
+            type="radio"
+            name="location"
+            id="location"
+            checked={location === "Studio"}
+            value="Studio"
+            onChange={(e) => {
+              setLocation(e.target.value);
+            }}
+          />
+          Studio
+        </label>
+        <label htmlFor="location">
+          <input
+            type="radio"
+            name="location"
+            id="location"
+            checked={location === "Online"}
+            value="Online"
+            onChange={(e) => {
+              setLocation(e.target.value);
+            }}
+          />
+          Online
+        </label>
+        <br></br>
+        <label>Instrument:</label>
+        <label htmlFor="instrument">
+          <input
+            type="radio"
+            name="instrument"
+            id="instrument"
+            value="Guitar"
+            checked={instrument === "Guitar"}
+            onChange={(e) => {
+              setInstrument(e.target.value);
+            }}
+          />
+          Guitar
+        </label>
+        <label htmlFor="instrument">
+          <input
+            type="radio"
+            name="instrument"
+            id="instrument"
+            value="Piano"
+            checked={instrument === "Piano"}
+            onChange={(e) => {
+              setInstrument(e.target.value);
+            }}
+          />
+          Piano
+        </label>
+        <label htmlFor="instrument">
+          <input
+            type="radio"
+            name="instrument"
+            id="instrument"
+            value="Voice"
+            checked={instrument === "Voice"}
+            onChange={(e) => {
+              setInstrument(e.target.value);
+            }}
+          />
+          Voice
+        </label>
+        <br></br>
+        <button onClick={updateBooking}> Edit Booking </button>
+      </div>
+      <div>
+        <h1>You currently have {mybookings.length} bookings </h1>
         Click {<Link to="/bookings/new">here</Link>} to make a new booking
       </div>
-    );
-  }
+    </>
+  );
 };
 
 export default AsyncAwait;
